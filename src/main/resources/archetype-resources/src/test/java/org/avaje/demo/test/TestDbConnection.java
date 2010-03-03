975e027fd32dfc2e838c1abb13ed5c92ddee552f
Paul Mendelson <Paul Mendelson@squirrel.(none)>
2010-03-03 07:57:29 -0500

    baseline ebean archtype

---
+++ src/main/resources/archetype-resources/src/test/java/org/avaje/demo/test/TestDbConnection.java
@@ -0,0 +1,27 @@
+#set( $symbol_pound = '#' )
+#set( $symbol_dollar = '$' )
+#set( $symbol_escape = '\' )
+package org.avaje.demo.test;
+import junit.framework.Assert;
+
+import org.junit.Test;
+
+import com.avaje.ebean.Ebean;
+import com.avaje.ebean.SqlRow;
+
+
+public class TestDbConnection {
+
+    @Test
+    public void test(){
+        
+        String sql = "select count(*) as cnt from b_testtable";
+        SqlRow row = Ebean.createSqlQuery(sql)
+            .findUnique();
+        
+        Integer count = row.getInteger("cnt");
+        
+        Assert.assertNotNull(count);
+        
+    }
+}
